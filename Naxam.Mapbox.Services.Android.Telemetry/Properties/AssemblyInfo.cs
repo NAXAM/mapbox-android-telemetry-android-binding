de780685e27854d52ec92dc9d94e8619d613eef4
Vu Duc Tuyen <tuyen@naxam.net>
2017-04-16 00:56:19 +0700

    Update assembly title

--- Naxam.Mapbox.Services.Android.Telemetry/Properties/AssemblyInfo.cs
+++ Naxam.Mapbox.Services.Android.Telemetry/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using Android.App;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Naxam.Mapbox.Sdk.MapboxAndroidTelemetry")]
+[assembly: AssemblyTitle("Naxam.Mapbox.Services.Android.Telemetry")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NAXAM CO.,LTD")]
@@ -18,8 +18,8 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyInformationalVersion("2.0.0.1")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Xamarin Binding Library for [Mapbox Android Telemetry](https://mvnrepository.com/artifact/com.mapbox.mapboxsdk/mapbox-android-telemetry)
 
 ```
-Install-Package Naxam.Mapbox.Sdk.MapboxAndroidTelemetry
+Install-Package Naxam.Mapbox.Services.Android.Telemetry
 ```
